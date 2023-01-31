613865059b1a99cfbfecdfa362c4ac0b8e3c5ea3
AdamWarfa <Adamwarfadk@gmail.com>
2023-01-31 13:20:15 +0100

    newFunction

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ alert("Hello Dat.js");
 console.log("Hello Dat.js");
 console.log("Hi Students!");
 console.log("This is your first JavaScript App 🎉");
+function sayHello() {
+  document.querySelector("h3").textContent =
+    "Hej " + document.querySelector("#name").value;
+}
+document.querySelector("button").addEventListener("click", sayHello);
