9e512135ba30e4e60f679dafd496011dcc573835
AdamWarfa <Adamwarfadk@gmail.com>
2023-03-09 15:28:48 +0100

    data types test

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 document.body.innerHTML += "Hello JavaScript!";
 // alert("Hello Dat.js");
-alert("Hello Dat.js");
-console.log("Hello Dat.js");
-console.log("Hi Students!");
-console.log("This is your first JavaScript App 🎉");
+// alert("Hello Dat.js");
+// console.log("Hello Dat.js");
+// console.log("Hi Students!");
+// console.log("This is your first JavaScript App 🎉");
+
 function sayHello() {
-  document.querySelector("h3").textContent =
-    "Man like " + document.querySelector("#name").value + " yeye";
+  const greeting = document.querySelector("h3");
+  let firstName = document.querySelector("#name").value;
+  let age = document.querySelector("#age").value;
+  let shoutOut = `Man like ${firstName} yeye, ${age} years old n dat`;
+
+  greeting.textContent = shoutOut;
 }
 document.querySelector("button").addEventListener("click", sayHello);
